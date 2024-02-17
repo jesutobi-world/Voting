9555ef1bf2da7e4b8d26167027883a975806dbca
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2024-02-17 11:17:02 +0100

    login ...

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import LoginImage from '/LoginImage.svg';
 const Login = () => {
   return (
-    <div className="bg-[#b4c5e4] h-auto  w-screen flex items-center justify-center p-5 lg:p-10">
+    <div className="bg-[#b4c5e4] h-auto  w-screen flex items-center justify-center px-2 py-5 lg:p-10">
       <div className="h-auto w-full lg:w-[80%] bg-white rounded-xl shadow-md flex flex-col-reverse lg:flex-row overflow-hidden">
         <div className='flex flex-col gap-5 w-full lg:w-1/2 px-5 py-10 lg:py-5 lg:px-16'>
           <div className='text-xl lg:text-2xl font-bold text-[#23233b]'>VoteWise</div>
@@ -29,13 +29,13 @@ const Login = () => {
             <hr className='w-auto h-[1px] bg-gray-300 flex-1'/>
           </div>
           <form action="#" className='w-full'>
-            <div className='w-full h-[45px] rounded-lg flex border border-solid border-gray-400 items-center px-4 gap-4 overflow-hidden mb-4'>
+            <div className='w-full h-[55px] lg:h-[45px] rounded-lg flex border border-solid border-gray-400 items-center px-4 gap-4 overflow-hidden mb-4'>
               <div className='h-full flex items-center'>
                 <FontAwesomeIcon icon={faEnvelope} className='text-gray-400 text-lg' />
               </div>
               <input type="text" placeholder='Email' className='h-full flex-1 outline-none bg-transparent border-none text-[#3c3744] text-lg flex items-center font-medium' />
             </div>
-            <div className='w-full h-[45px] rounded-lg flex border border-solid border-gray-400 items-center px-4 gap-4 overflow-hidden mb-4'>
+            <div className='w-full h-[55px] lg:h-[45px] rounded-lg flex border border-solid border-gray-400 items-center px-4 gap-4 overflow-hidden mb-4'>
               <div className='h-full flex items-center'>
                 <FontAwesomeIcon icon={faLock} className='text-gray-400 text-lg' />
               </div>
@@ -54,14 +54,14 @@ const Login = () => {
                 <Link to="/reset" className='text-blue-700 '>Forgot Password?</Link>
               </div>
             </div>
-            <button  className='w-full h-[45px] rounded-lg border border-solid border-blue-700 text-white text-xl font-semibold'
-            style={{backgroundColor: '#1e40af',}}
+            <button  className='w-full py-3 rounded-lg border border-solid border-blue-700 text-white text-xl font-semibold'
+            style={{backgroundColor: 'rgb(73 104 207)',}}
             type='submit'>Login</button>
           </form>
-          <div className='flex justify-center gap-1'>
+          <div className='flex justify-center'>
             <div>Don{"'"}t have an account?</div>
             <div>
-            <Link to="/signup">Create an account</Link>
+            <Link to="/signup" className='text-blue-700 pl-[2px]'>Create an account</Link>
             </div>
           </div>
         </div>
